656ff7e3285d30a4b52651d3cb25406ee06ebef8
shivamgupta002 <shivamgupta992909@gmail.com>
2023-10-20 21:42:57 +0530

    ADD decimal to binary conversion

---
+++ Loop & Bitwise Operator/BinaryToDecimal.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter number = ";
+    cin >> n;
+    int ans = 0, i = 0;
+    while (n != 0)
+    {
+        int bit = n % 10;
+        if (bit == 1)
+        {
+            ans = pow(2, i) + ans;
+        }
+        n = n / 10;
+        i++;
+        cout << ans << endl;
+    }
+    cout << "Final ans = " << ans << endl;
+}
