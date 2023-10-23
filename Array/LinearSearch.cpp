fe9b4e8e8622ade92823a07c2182ff15436989ac
shivamgupta002 <shivamgupta992909@gmail.com>
2023-10-23 20:07:20 +0530

    Add linear search  program

---
+++ Array/LinearSearch.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+bool linearSearch(int arr[], int size, int key)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == key)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+int main()
+{
+    int size, key;
+    int arr[100];
+
+    cout << "Enter the size of array : ";
+    cin >> size;
+
+    cout << "Enter the element of the array : ";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Enter the key to search for : ";
+    cin >> key;
+    if (linearSearch(arr, size, key))
+    {
+        cout << "Element found ";
+    }
+    else
+    {
+        cout << "No element found";
+    }
+}
