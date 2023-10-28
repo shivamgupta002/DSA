463cf6ebb45990a20a070c4b79f9a1cd5a6302fa
shivamgupta002 <shivamgupta992909@gmail.com>
2023-10-28 14:28:24 +0530

    Priority queue STL add

---
+++ STL/Priority_queue.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main()
+{
+    // max heap
+    priority_queue<int> maxi;
+
+    // min heap
+    priority_queue<int, vector<int>, greater<int>> mini;
+
+    // for maxi
+    maxi.push(1);
+    maxi.push(3);
+    maxi.push(2);
+    maxi.push(0);
+
+    int n = maxi.size();
+    for (int i = 0; i < n; i++)
+    {
+        cout << maxi.top() << " ";
+        maxi.pop();
+    }
+    cout << endl;
+
+    // for mini
+    mini.push(5);
+    mini.push(1);
+    mini.push(3);
+    mini.push(2);
+    mini.push(0);
+
+    int m = mini.size();
+    for (int i = 0; i < m; i++)
+    {
+        cout << mini.top() << " ";
+        mini.pop();
+    }
+    cout << endl;
+
+
+    cout<<"Empty or not => "<<mini.empty()<<endl;
+}
