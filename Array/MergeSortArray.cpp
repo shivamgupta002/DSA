bb1ae809c2c0af7ad207c29608c44fdfdf6cb5e9
shivamgupta002 <shivamgupta992909@gmail.com>
2023-10-30 12:49:52 +0530

    MergeSort Array

---
+++ Array/MergeSortArray.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void mergeSort(int arr1[], int n, int arr2[], int m, int arr3[])
+{
+    int i = 0, j = 0, k = 0;
+    while (i < n && j < m)
+    {
+        if (arr1[i] < arr2[j])
+        {
+            arr3[k] = arr1[i];
+            i++;
+        }
+        else
+        {
+            arr3[k] = arr2[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n)
+    {
+        arr3[k] = arr1[i];  
+        i++;
+        k++;
+    }
+    while (j < m)
+    {
+        arr3[k] = arr2[j];
+        j++;
+        k++;
+    }
+}
+void print(int arr[], int s)
+{
+    for (int i = 0; i < s; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    int arr1[5] = {1, 3, 5, 7, 9};
+    int arr2[3] = {2, 4, 6};
+    int arr3[8] = {0};
+    mergeSort(arr1, 5, arr2, 3, arr3);
+    print(arr3, 8);
+}
